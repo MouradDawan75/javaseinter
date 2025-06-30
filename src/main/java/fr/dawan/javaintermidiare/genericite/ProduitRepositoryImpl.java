ea8e333a3d5f14eef10fa2db426a2540db3f3b0f
Mourad <mmahrane@dawan.fr>
2025-06-30 11:23:20 +0200

    Jour1-Matin

---
+++ src/main/java/fr/dawan/javaintermidiare/genericite/ProduitRepositoryImpl.java
@@ -0,0 +1,18 @@
+package fr.dawan.javaintermidiare.genericite;
+
+public class ProduitRepositoryImpl implements IRepository<Produit>{
+    @Override
+    public void insert(Produit obj) {
+
+    }
+
+    @Override
+    public void delete(Produit obj) {
+
+    }
+
+    @Override
+    public void update(Produit obj) {
+
+    }
+}
